c2b952cb3e94f02bd4789a77c3b076cc694bf252
LasithEranga <lasitheranga1@gmail.com>
2022-03-17 00:48:44 +0530

    basic design and components

--- JavaBrick/src/javabrick/GamePlay.java
+++ JavaBrick/src/javabrick/GamePlay.java
@@ -28,7 +28,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener{
     private Timer timer;
     private int delay = 8;
     
-    private int playerX = 310;
+    private int playerX = 280;
     
     private int ballposX = 120;
     private int ballposY = 350;
@@ -46,26 +46,63 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener{
     public void paint(Graphics g){
         g.setColor(Color.black);
         g.fillRect(1, 1, 692, 592);
+        
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);        
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(0, 0, 3, 592);
+
+        //the paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        //the ball
+        g.setColor(Color.yellow);
+        g.fillOval(ballposX, ballposY, 20, 20);
     }
     
     @Override
     public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if(playerX >= 600){
+                playerX = 600;
+            } else{
+                moveRight();
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            if(playerX < 10){
+                playerX = 10;
+            } else{
+                moveLeft();
+            }
+        }
+    }
+    
+    public void moveRight(){
+        play = true;
+        playerX += 20;
+    }
+    
+    public void moveLeft(){
+        play = true;
+        playerX -= 20;
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        timer.start();
+        repaint();
     }
     
 }
